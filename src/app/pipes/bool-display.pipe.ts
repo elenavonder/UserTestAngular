28ab11eb98e793fe37dfdfc86e32017f4bf18e58
Elena Vonder Meulen <evonder13@gmail.com>
2020-11-13 09:53:49 -0500

    added bool pipe class to code

--- src/app/pipes/bool-display.pipe.ts
+++ src/app/pipes/bool-display.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BoolDisplayPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(aBoolean: boolean,): string {
+    return aBoolean ? "YES" : "NO";
   }
 
 }
